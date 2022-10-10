3afc6f140e53b39674f8ef935c9004fa00f9cb48
sanketupadhyay <sanket.upadhyay@neebal.com>
2022-10-10 07:50:18 +0530

    [REMOVE] client

--- frontend/src/app/interfaces/movie.ts
+++
@@ -1,8 +0,0 @@
-export interface Movie {
-    id?: number;
-    name: string;
-    category: string;
-    rating: number;
-    created_at: Date;
-    updated_at: Date;
-}
--- frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  socketUrl: 'http://localhost:3000'
-};

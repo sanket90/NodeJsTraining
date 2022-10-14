db0c3695fc3cf71bbe4faffaa5cf555bd70a9363
sanketupadhyay <sanket.upadhyay@neebal.com>
2022-10-14 09:14:03 +0530

    [Add] typescript demo

---
+++ nodeTs/src/index.ts
@@ -0,0 +1,20 @@
+let isValid;
+
+
+isValid = true;
+
+console.log(isValid);
+
+
+
+interface User {
+    name: string;
+    age: number;
+    salary?: number;
+}
+
+const user: User = {
+    name: "xyz",
+    age: 15
+}
+
